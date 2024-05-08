d2b5ac95584a46dea684d3c0ded06a591c9aaf6d
sohyeon <xxsoxx0621@naver.com>
2024-05-08 23:03:06 +1000

    first commit

---
+++ phoneBook/src/phoneBook/Contact.java
@@ -0,0 +1,49 @@
+package phoneBook;
+
+public class Contact {
+	private String name;
+	private String birthday;
+	private String phone;
+	private String address;
+	
+	public Contact(String name, String birthday, String phone, String address) {
+		this.name = name;
+		this.birthday = birthday;
+		this.phone = phone;
+		this.address = address;
+	}
+	public String getName() {
+		return "";
+	}
+	
+	public String getBirthday() {
+		return "";
+	}
+	
+	public String getPhone() {
+		return "";
+	}
+	
+	public String getAddress() {
+		return "";
+	}
+	
+	public void setName(String name) {
+		return;
+	}
+	
+	public void setBirthday(String birthday) {
+		return;
+	}
+	
+	public void setPhone(String phone) {
+		return;
+	}
+	
+	public void setAddress(String address) {
+		return;
+	}
+	
+	
+
+}
---
+++ phoneBook/src/phoneBook/Main.java
@@ -0,0 +1,9 @@
+package phoneBook;
+
+import java.io.*;
+
+public class Main {
+	public static void main(String[] args) {
+		
+	}
+}
